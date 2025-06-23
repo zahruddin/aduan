3a70413f70a485aa600e5b5b438cbb0f6cad0881
zahruddin <zahruddin.fanani17@gmail.com>
2025-06-23 14:51:27 +0700

    memperbaiki

--- profil.php
+++ profil.php
@@ -79,7 +79,7 @@ $stmt->close();
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Dashboard Admin</title>
+  <title>Profil Admin</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   <style>
     body {
