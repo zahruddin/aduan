f8df3c37671c34c21ddf4a48dd3b3170015d13f5
zahruddin <zahruddin.fanani17@gmail.com>
2025-06-23 14:53:40 +0700

    memperbaiki

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,12 @@
 <?php
-// $host = "localhost";
-// $user = "seefanmy_aduan";
-// $pass = "v8,sB6tu@a1~luw)";
-// $db   = "seefanmy_aduan";
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "pengaduan_masyarakat";
+$user = "seefanmy_aduan";
+$pass = "v8,sB6tu@a1~luw)";
+$db   = "seefanmy_aduan";
+// $host = "localhost";
+// $user = "root";
+// $pass = "";
+// $db   = "pengaduan_masyarakat";
 
 $koneksi = new mysqli($host, $user, $pass, $db);
 if ($koneksi->connect_error) {
